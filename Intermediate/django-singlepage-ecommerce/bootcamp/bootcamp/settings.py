432ce22cd9e3197651c7a25d686d9445562b2e7b
Pujan Thapa <iampujan@outlook.com>
2020-10-11 14:41:37 +0545

    Update settings.py

--- Intermediate/django-singlepage-ecommerce/bootcamp/bootcamp/settings.py
+++ Intermediate/django-singlepage-ecommerce/bootcamp/bootcamp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$9f1-6sh1&o1i(9l+#_647z#*b)z54mspj6w3&_t*_yv#v&ze*'
+SECRET_KEY = config.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
