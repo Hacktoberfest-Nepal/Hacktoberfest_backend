e50c07989504003e9be5db07930c349a2a788462
Nirjal Paudel <Nirjal Paudel>
2020-10-13 18:28:33 +0545

    Made a simple chatapp

---
+++ Intermediate/Chat App/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const sockets = require("socket.io");
+
+app = express()
+app.set("view engine","ejs")
+app.use(
+    express.static(path.join(__dirname,"assets"))
+);
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+const server = app.listen(3000,()=>console.log("Listening on port "+3000))
+
+const io = sockets(server);
+
+io.on("connection",(socket)=>{
+    socket.emit("message", {
+        username:"Nodejs Server",
+        message: "Welcome to nodejs Chat app. Your id is "+socket.id,
+        pic: "https://previews.123rf.com/images/goodzone95/goodzone951803/goodzone95180300023/96668201-chatbot-icon-cute-robot-working-behind-laptop-modern-bot-sign-design-smiling-customer-service-robot-.jpg"
+    })
+    socket.on("chatmsg", (data) => {
+        socket.broadcast.emit("message", {
+            message: data.message,
+            username:data.username,
+            pic: data.pic
+        });
+    })
+})
---
+++ Intermediate/Chat App/assets/js/sockets.js
@@ -0,0 +1,25 @@
+var name = prompt("ENTER YOUR NAME");
+var text = document.getElementById("chat-text");
+    
+
+const socket = io("http://localhost:3000");
+
+
+var form = document.getElementById("form");
+
+//SOCKETS
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(text.value);
+    socket.emit("chatmsg", {
+        message: text.value,
+        username: name,
+        pic: me.avatar
+    });
+    text.value = "";
+});
+
+socket.on("message", (data) => {
+    insertChat(data.username,data.message,undefined,data.pic);
+})
