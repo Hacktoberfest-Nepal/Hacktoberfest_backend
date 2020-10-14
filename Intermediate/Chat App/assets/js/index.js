7ade460823a8379af97a44394d00b61cb655d104
Nirjal Paudel <Nirjal Paudel>
2020-10-14 14:05:34 +0545

    formattedusing preettier

--- Intermediate/Chat App/assets/js/index.js
+++ Intermediate/Chat App/assets/js/index.js
@@ -1,84 +1,76 @@
 var me = {};
-me.avatar = "https://image.freepik.com/free-vector/profile-icon-male-avatar-hipster-man-wear-headphones_48369-8728.jpg";
+me.avatar =
+  "https://image.freepik.com/free-vector/profile-icon-male-avatar-hipster-man-wear-headphones_48369-8728.jpg";
 
 var you = {};
 
-
-
 function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-}            
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
 
 //-- No use time. It is a javaScript effect.
 function insertChat(who, text, time, pic = undefined) {
-    if (pic) {
-        you.avatar = pic;
-    }
-    if (time === undefined){
-        time = 0;
-    }
-    var control = "";
-    var date = formatAMPM(new Date());
-    
-    if (who == "me"){
-        control = '<li style="width:100%">' +
-                        '<div title="You sent this" class="msj macro">' +
-                        '<div class="avatar"><img class="img-circle" src="'+ me.avatar +'" /></div>' +
-                            '<div class="text text-l">' +
-                                '<p>'+ text +'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</li>';                    
-    }else{
-        control = '<li style="width:100%;">' +
-                        '<div class="msj-rta macro" title="Sent by '+who+'">' +
-                            '<div class="text text-r">' +
-                                '<p>'+text+'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" src="'+you.avatar+'" /></div>' +                                
-                  '</li>';
-    }
-    setTimeout(
-        function(){                        
-            $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
-        }, time);
-    
+  if (pic) {
+    you.avatar = pic;
+  }
+  if (time === undefined) {
+    time = 0;
+  }
+  var control = "";
+  var date = formatAMPM(new Date());
+
+  if (who == "me") {
+    control = `<li style="width:100%">
+                        <div title="You sent this" class="msj macro"> 
+                        <div class="avatar"><img class="img-circle" src="${me.avatar}" /></div> 
+                            <div class="text text-l"> 
+                                <p>${text}</p> 
+                                <p><small>${date}</small></p> 
+                            </div> 
+                        </div> 
+                    </li>`;
+  } else {
+    control = `<li style="width:100%;"> 
+                        <div class="msj-rta macro" title="Sent by ${who}"> 
+                            <div class="text text-r"> 
+                                <p>${text}</p> 
+                                <p><small>${date}</small></p> 
+                            </div> 
+                        <div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" src="${you.avatar}" /></div>                           
+                  </li>`;
+  }
+  setTimeout(function () {
+    $("ul").append(control).scrollTop($("ul").prop("scrollHeight"));
+  }, time);
 }
 
-function resetChat(){
-    $("ul").empty();
+function resetChat() {
+  $("ul").empty();
 }
 
-$(".mytext").on("keydown", function(e){
-    if (e.which == 13){
-        var text = $(this).val();
-        if (text !== ""){
-            insertChat("me", text);              
-        }
+$(".mytext").on("keydown", function (e) {
+  if (e.which == 13) {
+    var text = $(this).val();
+    if (text !== "") {
+      insertChat("me", text);
     }
+  }
 });
 
-$('body > div > div > div:nth-child(2) > span').click(function(){
-    $(".mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
-})
+$("body > div > div > div:nth-child(2) > span").click(function () {
+  $(".mytext").trigger({ type: "keydown", which: 13, keyCode: 13 });
+});
 
 //-- Clear Chat
 resetChat();
 
 //-- Print Messages
-//insertChat("Hari", "Hello Tom...");  
-// insertChat("you", "Hi, Pablo", 1500);
 // insertChat("me", "What would you like to talk about today?", 3500);
 // insertChat("you", "Tell me a joke",7000);
-// insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
-// insertChat("you", "LOL", 12000);
-// insertChat("you", "LOLllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll", 12000);
--- Intermediate/Chat App/assets/js/sockets.js
+++ Intermediate/Chat App/assets/js/sockets.js
@@ -1,26 +1,24 @@
 var name = prompt("ENTER YOUR NAME");
 var text = document.getElementById("chat-text");
-    
 
 //const socket = io("http://localhost:3000");
 const socket = io("https://n1chat.herokuapp.com/");
-
 
 var form = document.getElementById("form");
 
 //SOCKETS
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log(text.value);
-    socket.emit("chatmsg", {
-        message: text.value,
-        username: name,
-        pic: me.avatar
-    });
-    text.value = "";
+  event.preventDefault();
+  console.log(text.value);
+  socket.emit("chatmsg", {
+    message: text.value,
+    username: name,
+    pic: me.avatar,
+  });
+  text.value = "";
 });
 
 socket.on("message", (data) => {
-    insertChat(data.username,data.message,undefined,data.pic);
-})
+  insertChat(data.username, data.message, undefined, data.pic);
+});
